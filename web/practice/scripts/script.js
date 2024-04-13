09ef942a505be9450cf28ad97f4fec91141152c9
MarkSharmy <138826469+MarkSharmy@users.noreply.github.com>
2024-04-13 14:30:33 +0200

    refactor!: Create vanilla directory web files
    Created new files for the vanilla directory.
    Included in the directory are:
    index.html
    admin.html
    images folder
    css files

--- web/practice/scripts/script.js
+++ web/practice/scripts/script.js
@@ -1,3 +1,5 @@
+window.onload = init;
+
 const allSideMenu = document.querySelectorAll("#sidebar .side-menu.top li a");
 
 allSideMenu.forEach( item => {
@@ -23,3 +25,60 @@ console.log(sidebar);
 menubar.addEventListener("click", () => {
     sidebar.classList.toggle("hide");
 });
+
+function init()
+{
+    setCurrentDate();
+}
+
+
+function setCurrentDate()
+{
+    let currentDate = new Date();
+
+    let dateIndex = currentDate.getDate();
+    let date = 0;
+
+    if (dateIndex === 1)
+        date = dateIndex + "st";
+    else if (dateIndex === 2)
+        date = dateIndex + "nd"
+    else if (dateIndex === 3)
+        date = dateIndex + "rd"
+    else
+        date = dateIndex + "th"
+
+    let monthIndex = currentDate.getMonth();
+    let month = 0;
+
+    switch(monthIndex)
+    {
+        case 0: month = "January";
+        break;
+        case 1: month = "February";
+        break;
+        case 2: month = "March";
+        break;
+        case 3: month = "April";
+        break;
+        case 4: month = "May";
+        break;
+        case 5: month = "June";
+        break;
+        case 6: month = "July";
+        break;
+        case 7: month = "August";
+        break;
+        case 8: month = "September";
+        break;
+        case 9: month = "October";
+        break;
+        case 10: month = "November";
+        break;
+        case 11: month = "December";
+        break;
+    }
+
+    let dayTitle = document.getElementById("currentDate");
+    dayTitle.innerHTML = `Today - ${date} of ${month}`;
+}
