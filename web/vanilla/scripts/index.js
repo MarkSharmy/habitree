8ea8fe185437191da3451635968139c242195787
MarkSharmy <138826469+MarkSharmy@users.noreply.github.com>
2024-04-17 21:05:59 +0200

    fix: debug server error

--- web/vanilla/scripts/index.js
+++ web/vanilla/scripts/index.js
@@ -1,3 +1,79 @@
+import Routes from "./routes/Routes.js";
 
 
-console.log("routes");
+
+//Defacto routing function
+async function router()
+{
+    //const routes = Routes.getRoutes();
+    
+    const routes = [
+        {path: "/dashboard", view: Dashboard},
+        {path: "/dashboard/calendar", view: Calendar},
+        {path: "/dashboard/alltasks", view: AllTasks},
+        {path: "/dashboard/analytics", view: Analytics},
+        {path: "/dashboard/projects", view: Projects},
+        {path: "/dashboard/profile", view: Profile},
+        {path: "/dashboard/settings", view: Settings},
+
+    ];
+
+    const potentialMatches = routes.map((route) => {
+
+        return {
+            route: route,
+            result: location.pathname.match(pathToRegex(route.path))
+        };
+
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
+
+    if (!match)
+    {
+        match = {
+            route: Routes.get404(),
+            result: [location.pathname]
+        };
+    }
+
+    const view = new match.route.view(getParams(match));
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+}
+
+function navigateTo(url)
+{
+    history.pushState(null, null, url);
+    router();
+}
+
+function getParams(match)
+{
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    console.log("Array:",Array.from(match.route.path.matchAll(/:(\w+)/g)));
+
+    return Object.fromEntries(keys.map((key, i) => {
+        return [key, values[i]];
+    }));
+}
+
+function pathToRegex(path)
+{
+    return new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+}
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router();
+});
--- web/vanilla/scripts/routes/routes.js
+++ web/vanilla/scripts/routes/routes.js
@@ -1,1 +1,19 @@
-export default routes = [];
+export default class {
+
+    static routes = [];
+
+    static addRoute(route)
+    {
+        this.routes.push(route);
+    }
+
+    static getRoutes()
+    {
+        return this.routes;
+    }
+
+    static get404()
+    {
+        return {path: "/404", view: () => console.log("page not found")};
+    }
+}
